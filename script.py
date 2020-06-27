af05055c5da59d6562af10031ae7960fb06f3402
petrerobert01 <r.petregrigorie@gmail.com>
2020-06-28 01:25:38 +0300

    Added interest finder logic

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
-destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil" "Cairo, Egypt"]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 def get_destination_index(destination):
     destination_index = destinations.index(destination)
     return destination_index
 
-print(get_destination_index("Los Angeles, USA"))
+#print(get_destination_index("Los Angeles, USA"))
 
 def get_traveler_location(traveler):
     traveler_destination = traveler[1]
@@ -13,19 +13,19 @@ def get_traveler_location(traveler):
     return traveler_destination_index
 
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
+#print(test_destination_index)
 
 attractions = [[] for destination in destinations]
-print (attractions)
 
-def add_attractions(destination, attraction):
+
+def add_attraction(destination, attraction):
     try:
         destination_index = get_destination_index(destination)
         attractions_for_destinations = attractions[destination_index].append(attraction)
     except SyntaxError:
         return
 
-add_attractions("Los Angeles, USA",['Venice Beatch',['beach']])
+add_attraction("Los Angeles, USA",['Venice Beatch',['beach']])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
 add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
@@ -36,3 +36,20 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+###print (attractions)
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+
+    for possible_attraction in attractions_in_city:
+        attraction_tags = possible_attraction[1]
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+
+    return attractions_with_interest
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
